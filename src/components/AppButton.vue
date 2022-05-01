def58616ccc5b3b44fb90e7ad2c14eceb43ab01d
Timur Kostenko <ktim8168@mail.ru>
2022-05-01 15:31:07 +0300

    Remove unnecessary code

--- src/components/AppButton.vue
+++ src/components/AppButton.vue
@@ -8,11 +8,5 @@
 
 <style lang="scss" module>
 .wrapper {
-  @apply flex justify-center items-center bg-orange sm:text-md text-sm tracking-[0.135em] uppercase text-pure-white rounded-full p-[0.95rem] font-bold;
-  @apply leading-none;
-
-  &:hover {
-    @apply bg-pure-white text-orange;
-  }
 }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,19 +7,7 @@ module.exports = {
     colors: colorStyles,
     extend: {
       fontFamily: {
-        sans: ['Overpass', ...fontFamily.sans],
-      },
-      fontSize: {
-        sm: ['0.875rem', '1.4rem'],
-        md: ['0.9375rem', '1.5rem'],
-        '3xl': ['1.75rem', '2.25rem'],
-      },
-      backgroundImage: {
-        'black-gradient':
-          'radial-gradient(98.96% 98.96% at 50% 0%, #232A34 0%, #181E27 100%);',
-      },
-      borderRadius: {
-        '4xl': '1.875rem',
+        sans: [...fontFamily.sans],
       },
       maxWidth: {
         auto: 'auto',
